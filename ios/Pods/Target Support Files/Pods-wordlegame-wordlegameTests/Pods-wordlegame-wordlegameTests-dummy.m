3424920e434a0c7968d6ad2d5b8083b54df13569
Carlos Petit <ca.alberto.p@gmail.com>
2022-02-25 23:34:06 -0300

    update gitignore file
    .gitignorefile has ben modify and repo cache are clean

--- ios/Pods/Target Support Files/Pods-wordlegame-wordlegameTests/Pods-wordlegame-wordlegameTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_wordlegame_wordlegameTests : NSObject
-@end
-@implementation PodsDummy_Pods_wordlegame_wordlegameTests
-@end
--- ios/Pods/Target Support Files/Pods-wordlegame/Pods-wordlegame-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_wordlegame : NSObject
-@end
-@implementation PodsDummy_Pods_wordlegame
-@end
