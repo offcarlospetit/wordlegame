17299bd31f17f34a75f9d76103a6f1f6a4bdc71b
Carlos Petit <ca.alberto.p@gmail.com>
2023-01-16 22:58:49 -0500

    wip: add supabase realtime config

--- src/utils/initSupBase.ts
+++ src/utils/initSupBase.ts
@@ -5,11 +5,18 @@ import { createClient } from '@supabase/supabase-js';
 export const SUPABASE_URL = "https://naxyowccndddojeiceuq.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5heHlvd2NjbmRkZG9qZWljZXVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzIxODMzNTIsImV4cCI6MTk4Nzc1OTM1Mn0.DWGgfVFpUpcpmx4WMPZ0dUZyK84h6Gj8DFSLNBMUBpo";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     // @ts-ignore
     localStorage: AsyncStorage as any,
     auth: {
         autoRefreshToken: true,
         persistSession: true,
     },
+    realtime: {
+        params: {
+            eventsPerSecond: 10,
+        },
+    },
 });
+
+export default supabase;
