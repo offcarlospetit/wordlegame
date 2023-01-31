710f1967a0973a00c10446ee148dee60fe565860
Carlos Petit <ca.alberto.p@gmail.com>
2023-01-30 23:19:35 -0500

    wip: update avatar component

--- src/ui-kit/components/Avatar/Avatar.tsx
+++ src/ui-kit/components/Avatar/Avatar.tsx
@@ -2,15 +2,27 @@ import React from 'react';
 import Box from '../Box/Box';
 import Card from '../Card/Card';
 import Text from '../Text/Text';
+import { Image, ImageStyle, TouchableOpacity } from 'react-native';
 
-type Props = {};
+type Props = {
+    source?: any;
+    imageStyle?: ImageStyle;
+    onPress?: () => void;
+    isSelected?: boolean;
+};
 
-const Avatar: React.FC<Props> = ({ }) => {
+const Avatar: React.FC<Props> = ({ source, imageStyle, onPress, isSelected }) => {
     return (
         <Box>
-            <Card variant="roundedAvatar" flex={1}>
-                <Text variant="cellText">dadasdasL</Text>
-            </Card>
+            <TouchableOpacity onPress={() => onPress ? onPress() : null}>
+                <Card variant={isSelected ? "roundedAvatarSelected" : "roundedAvatar"} >
+                    {
+                        source ?
+                            <Image resizeMode='cover' source={source} style={[{ width: 55, height: 55 }, imageStyle]} /> :
+                            <Text variant="cellText">A</Text>
+                    }
+                </Card>
+            </TouchableOpacity>
         </Box>
     );
 };
