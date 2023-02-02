7c8cfc877d152f2f70a85c28d9ff67fdca5e228a
Carlos Petit <ca.alberto.p@gmail.com>
2023-02-01 22:20:29 -0500

    wip: update register screen

--- src/user/containers/Register.tsx
+++ src/user/containers/Register.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react';
-import { FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { Avatar, Box, Button, Card, Text, TextInput } from '../../ui-kit';
+import React from 'react';
+import { FlatList } from 'react-native';
+import { Avatar, BasicHeader, Box, Button, Container, Text, TextInput } from '../../ui-kit';
 import { SocialIcon } from '@rneui/themed';
 import useLogin from '../hooks/useLogin';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { UserStackParams } from '../navigation';
 
-type Props = {};
-const Register: React.FC<Props> = ({ }) => {
+export interface RegisterProps extends NativeStackScreenProps<UserStackParams, 'Register'> { }
+
+const Register: React.FC<RegisterProps> = ({ navigation }) => {
     const {
         handleEmail,
         handleLogin,
@@ -23,74 +26,81 @@ const Register: React.FC<Props> = ({ }) => {
         avatar,
     } = useLogin();
 
-    console.log({ error });
-
+    const handleNavigation = () => {
+        navigation.navigate('Intro');
+    };
     return (
-        <Box flex={1} paddingHorizontal="m" justifyContent="center">
-            <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
-                <TextInput
-                    variant="regular"
-                    onChangeText={handleName}
-                    value={name}
-                    placeholder="Your Name"
-                    autoCapitalize={'words'}
-                />
-            </Box>
-            <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
-                <TextInput
-                    variant="regular"
-                    onChangeText={handleUsername}
-                    value={username}
-                    placeholder="your username"
-                    autoCapitalize={'none'}
-                />
-            </Box>
-            <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
-                <TextInput
-                    variant="regular"
-                    onChangeText={handleEmail}
-                    value={email}
-                    placeholder="email@address.com"
-                    autoCapitalize={'none'}
-                />
-            </Box>
-            <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
-                <TextInput
-                    variant="regular"
-                    onChangeText={handlePassword}
-                    value={password}
-                    secureTextEntry={true}
-                    placeholder="Password"
-                    autoCapitalize={'none'}
-                />
-            </Box>
-            <Box marginTop="m" paddingBottom="xs" alignSelf="stretch">
-                <Text variant="detailTitle" textAlign="center" marginVertical="xs">
-                    Pick your Avatar
-                </Text>
-                <FlatList
-                    horizontal
-                    showsHorizontalScrollIndicator={false}
-                    keyExtractor={(item) => item.index.toString()}
-                    extraData={avatars}
-                    data={avatars}
-                    renderItem={({ item, index }) => {
-                        return (
-                            <Box marginHorizontal="xs" flex={1} padding="s">
-                                <Avatar source={item.item} isSelected={item.isSelected} onPress={() => {
-                                    handleAvatar(index);
-                                }} />
-                            </Box>
-                        );
-                    }} />
-            </Box>
-            <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
-                {error ? <Text variant="danger">{error}</Text> : null}
-            </Box>
-            <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
-                <Button variant="success" label="Sign Up" onPress={() => handleLogin('SIGNUP', email, password, avatar)} />
-            </Box>
-            {/* 
+        <Container>
+            <BasicHeader handleNavigation={handleNavigation} />
+            <Box flex={1} paddingHorizontal="m" justifyContent="center">
+
+                <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
+                    <Text variant="title">Sign Up</Text>
+                </Box>
+                <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
+                    <TextInput
+                        variant="regular"
+                        onChangeText={handleName}
+                        value={name}
+                        placeholder="Your Name"
+                        autoCapitalize={'words'}
+                    />
+                </Box>
+                <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
+                    <TextInput
+                        variant="regular"
+                        onChangeText={handleUsername}
+                        value={username}
+                        placeholder="your username"
+                        autoCapitalize={'none'}
+                    />
+                </Box>
+                <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
+                    <TextInput
+                        variant="regular"
+                        onChangeText={handleEmail}
+                        value={email}
+                        placeholder="email@address.com"
+                        autoCapitalize={'none'}
+                    />
+                </Box>
+                <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
+                    <TextInput
+                        variant="regular"
+                        onChangeText={handlePassword}
+                        value={password}
+                        secureTextEntry={true}
+                        placeholder="Password"
+                        autoCapitalize={'none'}
+                    />
+                </Box>
+                <Box marginTop="m" paddingBottom="xs" alignSelf="stretch">
+                    <Text variant="detailTitle" textAlign="center" marginVertical="xs">
+                        Pick your Avatar
+                    </Text>
+                    <FlatList
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        keyExtractor={(item) => item.index.toString()}
+                        extraData={avatars}
+                        data={avatars}
+                        renderItem={({ item, index }) => {
+                            return (
+                                <Box marginHorizontal="xs" flex={1} padding="s">
+                                    <Avatar source={item.item} isSelected={item.isSelected} onPress={() => {
+                                        handleAvatar(index);
+                                    }} />
+                                </Box>
+                            );
+                        }} />
+                </Box>
+                <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
+                    {error ? <Text variant="danger">{error}</Text> : null}
+                </Box>
+                <Box marginTop="l" paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
+                    <Button variant="success" label="Sign Up" onPress={() => handleLogin('SIGNUP', email, password, avatar)} />
+                </Box>
+                {/* 
             <Text variant="detailTitle" style={{ textAlign: 'center', marginTop: 20 }}>
                 or
             </Text>
@@ -120,20 +130,10 @@ const Register: React.FC<Props> = ({ }) => {
                     }}
                 />
             </Box> */}
-        </Box>
+            </Box>
+        </Container>
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        marginTop: 40,
-        padding: 20,
-    },
-    verticallySpaced: {
-        paddingTop: 4,
-        paddingBottom: 4,
-        alignSelf: 'stretch',
-    },
-});
 
 export default Register;
