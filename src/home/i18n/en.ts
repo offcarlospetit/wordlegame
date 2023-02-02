d919131339be3a3227244b2073d30c5f1d763feb
Carlos Petit <ca.alberto.p@gmail.com>
2023-02-01 23:08:09 -0500

    wip: create dictionary for english

---
+++ src/home/i18n/en.ts
@@ -0,0 +1,8 @@
+import HomeStrings from "./index";
+
+export const english = {
+    [HomeStrings.Home.home]: "Game",
+    [HomeStrings.Home.homeBlockGame]: "Block Game",
+    [HomeStrings.Home.homeTitle]: "Wordle Game",
+    [HomeStrings.Home.homeBlockGameDescription]: "Your next DailyWord will be available at"
+};
