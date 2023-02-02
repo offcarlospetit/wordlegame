8bf3f695d76c591ea2ae31477c02b7080f00ab1e
Carlos Petit <ca.alberto.p@gmail.com>
2023-02-01 23:08:28 -0500

    wip: create spanish dictionary  for home screen

---
+++ src/home/i18n/es.ts
@@ -0,0 +1,7 @@
+import HomeStrings from "./index";
+
+export const spanish = {
+    [HomeStrings.Home.home]: "Juego",
+    [HomeStrings.Home.homeBlockGame]: "Juego Bloqueado",
+    [HomeStrings.Home.homeBlockGameDescription]: "Tu proxima palabra diaria estara disponible a las",
+};
