d5dbce2d78a748542fbe8edd015605a04066421e
Carlos Petit <ca.alberto.p@gmail.com>
2023-02-04 16:56:31 -0500

    wip: update home i18n

--- src/home/i18n/en.ts
+++ src/home/i18n/en.ts
@@ -4,5 +4,5 @@ export const english = {
     [HomeStrings.Home.home]: "Game",
     [HomeStrings.Home.homeBlockGame]: "Block Game",
     [HomeStrings.Home.homeTitle]: "Wordle Game",
-    [HomeStrings.Home.homeBlockGameDescription]: "Your next DailyWord will be available at"
+    [HomeStrings.Home.homeBlockGameDescription]: "Your next DailyWord will be available at "
 };
--- src/home/i18n/es.ts
+++ src/home/i18n/es.ts
@@ -3,5 +3,5 @@ import HomeStrings from "./index";
 export const spanish = {
     [HomeStrings.Home.home]: "Juego",
     [HomeStrings.Home.homeBlockGame]: "Juego Bloqueado",
-    [HomeStrings.Home.homeBlockGameDescription]: "Tu proxima palabra diaria estara disponible a las",
+    [HomeStrings.Home.homeBlockGameDescription]: "Tu proxima palabra diaria estara disponible a las ",
 };
