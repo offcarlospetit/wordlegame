31ebd28010b6a6b89d005d9f980c02e69f9a434f
Carlos Petit <ca.alberto.p@gmail.com>
2023-02-04 16:55:28 -0500

    wip: update login

--- src/user/containers/Login.tsx
+++ src/user/containers/Login.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { BasicHeader, Box, Button, Container, TextInput } from '../../ui-kit';
+import { BasicHeader, Box, Button, Container, Text, TextInput } from '../../ui-kit';
 import useLogin from '../hooks/useLogin';
 import SocialButton from '../components/SocialButton';
 
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { UserStackParams } from '../navigation';
+import { Keyboard, KeyboardAvoidingView, Pressable } from 'react-native';
+import S from '../../i18n';
 
 export interface LoginProps extends NativeStackScreenProps<UserStackParams, 'Login'> { }
 
@@ -19,31 +21,40 @@ const Login: React.FC<LoginProps> = ({ navigation }) => {
     <Container>
 
       <BasicHeader handleNavigation={handleNavigation} />
-
+      {/* <Pressable onPress={() => Keyboard.dismiss()} style={{ flex: 1 }}>
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding" keyboardVerticalOffset={0}> */}
       <Box flex={1} paddingHorizontal="m" justifyContent="center">
-        <Box paddingTop="xs" paddingBottom="xs" alignSelf="stretch" marginTop="l">
-          <TextInput
-            variant="regular"
-            onChangeText={handleEmail}
-            value={email}
-            placeholder="email@address.com"
-            autoCapitalize={'none'}
-          />
+        <Box flex={1} justifyContent="center" paddingBottom="xl">
+          <Text variant="title">{S.User.userLogin}</Text>
         </Box>
-        <Box paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
-          <TextInput
-            variant="regular"
-            onChangeText={handlePassword}
-            value={password}
-            secureTextEntry={true}
-            placeholder="Password"
-            autoCapitalize={'none'}
-          />
+        <Box flex={2}>
+          <Box paddingTop="xs" paddingBottom="xs" alignSelf="stretch" marginTop="l">
+            <TextInput
+              variant="regular"
+              onChangeText={handleEmail}
+              value={email}
+              placeholder={S.User.userEmail}
+              autoCapitalize={'none'}
+            />
+          </Box>
+          <Box paddingTop="xs" paddingBottom="xs" alignSelf="stretch" >
+            <TextInput
+              variant="regular"
+              onChangeText={handlePassword}
+              value={password}
+              secureTextEntry={true}
+              placeholder={S.User.userPassword}
+              autoCapitalize={'none'}
+            />
+          </Box>
+          <Box paddingTop="xs" paddingBottom="xs" alignSelf="stretch" marginTop="l">
+            <Button onPress={() => handleLogin('LOGIN', email, password)} variant="loginButton" label={S.User.userLogin} />
+          </Box>
         </Box>
-        <Box paddingTop="xs" paddingBottom="xs" alignSelf="stretch" marginTop="l">
-          <Button onPress={() => handleLogin('LOGIN', email, password)} variant="loginButton" label="Log In" />
-        </Box>
-        {/* <Text textAlign="center" marginTop="l" variant="detailTitle" >
+      </Box>
+      {/* </KeyboardAvoidingView>
+        </Pressable> */}
+      {/* <Text textAlign="center" marginTop="l" variant="detailTitle" >
         or
       </Text>
       <Box alignItems="center">
@@ -51,7 +62,6 @@ const Login: React.FC<LoginProps> = ({ navigation }) => {
         <SocialButton onPress={() => {
         }} type="apple" title="Sign In with Apple" />
       </Box> */}
-      </Box>
     </Container>
   );
 };
